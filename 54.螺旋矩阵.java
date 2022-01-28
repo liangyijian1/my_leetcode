330362de1d09f3e4cfe69ee51e290433e108cc6e
yijian <2269260331@qq.com>
2022-01-28 18:09:26 +0800

    8

---
+++ 54.螺旋矩阵.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=54 lang=java
+ *
+ * [54] 螺旋矩阵
+ */
+
+// @lc code=start
+class Solution {
+
+    public static void main(String[] args) {
+        //int[][] k = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        System.out.println("k[0]");
+    }
+
+}
+// @lc code=end
+
